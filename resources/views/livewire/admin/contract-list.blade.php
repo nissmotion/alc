29b233b3140518d373f0ebea487e698223e9948d
Kevin Hood <nissmotion@gmail.com>
2023-05-26 15:59:22 -0400

    testing auto deploy

--- resources/views/livewire/admin/contract-list.blade.php
+++ resources/views/livewire/admin/contract-list.blade.php
@@ -20,11 +20,11 @@
                 <div class="flex w-full items-center justify-between space-x-6 p-6">
                     <div class="flex-1 truncate">
                         <div class="flex items-center space-x-3">
-                            <h3 class="truncate text-sm font-medium text-gray-900">Jane Cooper</h3>
+                            <h3 class="truncate text-sm font-medium text-gray-900">{{ $contract->equipment->description }}</h3>
                             <span
                                 class="inline-flex flex-shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">Admin</span>
                         </div>
-                        <p class="mt-1 truncate text-sm text-gray-500">Regional Paradigm Technician</p>
+                        <p class="mt-1 truncate text-sm text-gray-500">{{ $contract->term }}</p>
                     </div>
                     {{-- <img class="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300"
                         src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
